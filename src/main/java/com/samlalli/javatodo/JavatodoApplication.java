6f0ac712cea43f194da9a242d8f01842c024d237
Sam Lalli <2sam.lalli@gmail.com>
2021-07-12 15:22:14 -0600

    initial commit

---
+++ src/main/java/com/samlalli/javatodo/JavatodoApplication.java
@@ -0,0 +1,13 @@
+package com.samlalli.javatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavatodoApplication.class, args);
+    }
+
+}
